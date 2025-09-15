1e3ff838f77adaa24f9ff8b2537f74e591fdfb24
Tuğçe Kızıldağ <tugce.kutuk88@gmail.com>
2025-09-15 15:42:19 +0300

    Initial files added

---
+++ index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './src/pages/Home';
+import About from './src/pages/About';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to='/'>#VANLIFE</Link>
+        <nav>
+          <Link to='/about'>About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
