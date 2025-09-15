f74f4c2f715adeb2030a42a14dfb3aaaa6a1baa6
Tuğçe Kızıldağ <tugce.kutuk88@gmail.com>
2025-09-15 21:25:42 +0300

    VanDetail page added

--- index.jsx
+++ index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './src/pages/Home';
 import About from './src/pages/About';
 import Vans from './src/pages/Vans';
+import VanDetail from './src/pages/VanDetail';
 
 import './server';
 
@@ -11,7 +12,9 @@ function App() {
   return (
     <BrowserRouter>
       <header>
-        <Link to='/'>#VANLIFE</Link>
+        <Link className='site-logo' to='/'>
+          #VANLIFE
+        </Link>
         <nav>
           <Link to='/about'>About</Link>
           <Link to='/vans'>Vans</Link>
@@ -21,6 +24,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
