de9a0ed0d3625bc3eac36e7ef213ffdd7aa2dbd9
Tuğçe Kızıldağ <tugce.kutuk88@gmail.com>
2025-09-22 00:05:28 +0300

    some css issues fixed and filtering vans types functionality added

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const typeFilter = searchParams.get('type');
 
   useEffect(() => {
     fetch('/api/vans')
@@ -11,7 +14,11 @@ export default function Vans() {
       .then((data) => setVans(data.vans));
   }, []);
 
-  const vanElements = vans.map((van) => (
+  const displayedVans = typeFilter
+    ? vans.filter((van) => van.type === typeFilter)
+    : vans;
+
+  const vanElements = displayedVans.map((van) => (
     <div key={van.id} className='van-tile'>
       <Link to={`/vans/${van.id}`}>
         <img src={van.imageUrl} />
@@ -29,6 +36,20 @@ export default function Vans() {
   return (
     <div className='van-list-container'>
       <h1>Explore our van options</h1>
+      <div className='van-list-filter-buttons'>
+        <Link to='?type=simple' className='van-type simple'>
+          Simple
+        </Link>
+        <Link to='?type=luxury' className='van-type luxury'>
+          Luxury
+        </Link>
+        <Link to='?type=rugged' className='van-type rugged'>
+          Rugged
+        </Link>
+        <Link to='.' className='van-type clear-filters'>
+          Clear Filter
+        </Link>
+      </div>
       <div className='van-list'>{vanElements}</div>
     </div>
   );
